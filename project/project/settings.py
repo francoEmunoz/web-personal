f79cdda8a5e229b6a28501d596c67a5fee8977cb
francoEmunoz <munozfranco011@gmail.com>
2023-04-03 22:52:14 -0300

    add app about

--- project/project/settings.py
+++ project/project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portfolio.apps.PortfolioConfig'
+    'portfolio.apps.PortfolioConfig',
+    'about.apps.AboutConfig'
 ]
 
 MIDDLEWARE = [
