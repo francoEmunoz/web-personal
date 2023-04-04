9b69c9183b1037ed475f36c23276981f780b909f
francoEmunoz <munozfranco011@gmail.com>
2023-04-03 22:56:23 -0300

    model

---
+++ project/about/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Description(models.Model):
+    texto = models.TextField(verbose_name="Texto")
+    image = models.ImageField(verbose_name="Imagen", upload_to= 'projects')
+    link = models.URLField(null=True, blank=True, verbose_name="Link")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    modified = models.DateTimeField(auto_now=True, verbose_name="Modificado")
+    
+    class Meta:
+        verbose_name = "decripcion"
+        verbose_name_plural = "descripciones"
